5a1a0d131033e577fa94f0ab34cf9d7d346abdad
suhanar <suhanasiraj60@gmail.com>
2021-06-08 20:05:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if($(window).scrollTop()>0){
+            $('.top').show();
+
+        }else{
+            $('.top').hide();
+        }
+    })
+
+    //smooth scrolling
+
+    $('a[href*="#"]'.on('click',function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },500,
+        'linear'
+        )
+
+    }))
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
